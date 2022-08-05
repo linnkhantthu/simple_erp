96fcad7b6de91e2f6bd87055f58dee09fb964911
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-08-05 22:49:27 +0630

    finish adding add prouct form

--- lib/inventory/addProduct.dart
+++ lib/inventory/addProduct.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AddProduct extends StatefulWidget {
   const AddProduct({Key? key}) : super(key: key);
@@ -10,16 +11,32 @@ class AddProduct extends StatefulWidget {
 class _AddProductState extends State<AddProduct> {
   late final TextEditingController _id;
   late final TextEditingController _productName;
+  late final TextEditingController _contains;
+  late final String _unit;
+  late final TextEditingController _price;
+
   var _idErrorText = null;
   var _productNameErrorText = null;
-  String dropdownValue = 'One';
+  var _containsErrorText = null;
+  var _priceErrorText = null;
 
   @override
   void initState() {
     _id = TextEditingController();
     _productName = TextEditingController();
-
+    _contains = TextEditingController();
+    _price = TextEditingController();
+    _unit = "PCS";
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _id.dispose();
+    _productName.dispose();
+    _contains.dispose();
+    _price.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,6 +45,7 @@ class _AddProductState extends State<AddProduct> {
       child: SizedBox(
         width: 400,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             const Padding(
               padding: EdgeInsets.all(8),
@@ -38,82 +56,102 @@ class _AddProductState extends State<AddProduct> {
                 ),
               ),
             ),
-            SizedBox(
-              width: 400,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Padding(
                     padding: const EdgeInsets.all(8),
-                    child: SizedBox(
-                      width: 100,
-                      child: TextField(
-                        controller: _id,
-                        decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            hintText: "ID",
-                            errorText: _idErrorText),
-                      ),
+                    child: TextField(
+                      enabled: false,
+                      controller: _id,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: "ID",
+                          errorText: _idErrorText),
                     ),
                   ),
-                  Padding(
+                ),
+                Flexible(
+                  child: Padding(
                     padding: const EdgeInsets.all(8),
-                    child: SizedBox(
-                      width: 200,
-                      child: TextField(
-                        controller: _productName,
-                        decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            hintText: "Product Name",
-                            errorText: _productNameErrorText),
-                      ),
+                    child: TextField(
+                      controller: _productName,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: "Product Name",
+                          errorText: _productNameErrorText),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            SizedBox(
-              width: 400,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Padding(
                     padding: const EdgeInsets.all(8),
-                    child: SizedBox(
-                      width: 100,
-                      child: TextField(
-                        controller: _id,
-                        decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            hintText: "Contains",
-                            errorText: _idErrorText),
-                      ),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      controller: _contains,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: "Contains",
+                          errorText: _containsErrorText),
                     ),
                   ),
-                  Padding(
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: DropdownButton(
+                    value: _unit,
+                    items: <String>['PCS', 'KG', 'G', 'Feet']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        _unit = value!;
+                      });
+                    },
+                  ),
+                ),
+                Flexible(
+                  child: Padding(
                     padding: const EdgeInsets.all(8),
-                    child: SizedBox(
-                      width: 200,
-                      child: DropdownButton(
-                        value: dropdownValue,
-                        items: <String>['One', 'Two', 'Free', 'Four']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (String? value) {
-                          setState(() {
-                            dropdownValue = value!;
-                          });
-                        },
-                      ),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                          RegExp(r'(^\d*\.?\d*)'),
+                        ),
+                      ],
+                      controller: _price,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: "Price",
+                          errorText: _priceErrorText),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green)),
+                  child: const Text(
+                    "Add",
+                    style: TextStyle(color: Colors.black),
+                  )),
+            )
           ],
         ),
       ),
