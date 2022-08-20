64097c15c060189f073c9346a97931b1097c8702
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-08-20 20:47:17 +0630

    added datatable and textfield inside it on Purchase

--- lib/inventory/purchase.dart
+++ lib/inventory/purchase.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_erp/inventory/Objects/Product.dart';
 
 class Purchase extends StatefulWidget {
   const Purchase({Key? key}) : super(key: key);
@@ -8,6 +9,67 @@ class Purchase extends StatefulWidget {
 }
 
 class _PurchaseState extends State<Purchase> {
+  final List columnNames = [
+    "ID",
+    "Product Name",
+    "Contains",
+    "Unit",
+    "Price",
+    "Qty",
+    ""
+  ];
+  final TextStyle dataTableStyle = const TextStyle(fontSize: 30);
+  List<Product> products = [
+    const Product(
+        id: 1,
+        productName: "TestData",
+        contains: 10,
+        unit: "PCS",
+        price: 2500.0,
+        qty: 0)
+  ];
+  late List<DataRow> dataRows;
+
+  @override
+  void initState() {
+    dataRows = List<DataRow>.generate(
+      products.length,
+      (index) => DataRow(
+        cells: <DataCell>[
+          DataCell(Text(products[index].id.toString(), style: dataTableStyle)),
+          DataCell(Text(products[index].productName.toString(),
+              style: dataTableStyle)),
+          DataCell(
+              Text(products[index].contains.toString(), style: dataTableStyle)),
+          DataCell(
+              Text(products[index].unit.toString(), style: dataTableStyle)),
+          DataCell(
+              Text(products[index].price.toString(), style: dataTableStyle)),
+          DataCell(Text(products[index].qty.toString(), style: dataTableStyle)),
+          const DataCell(
+            Icon(Icons.delete_outline),
+          ),
+        ],
+      ),
+    );
+    dataRows.add(
+      const DataRow(
+        cells: <DataCell>[
+          DataCell(TextField()),
+          DataCell(TextField()),
+          DataCell(TextField()),
+          DataCell(TextField()),
+          DataCell(TextField()),
+          DataCell(TextField()),
+          DataCell(
+            Icon(Icons.delete_outline),
+          ),
+        ],
+      ),
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -26,7 +88,16 @@ class _PurchaseState extends State<Purchase> {
                     ),
                   ],
                 ),
-                Text("Form"),
+                DataTable(
+                  columns: List<DataColumn>.generate(
+                      columnNames.length,
+                      (index) => DataColumn(
+                              label: Text(
+                            columnNames[index],
+                            style: dataTableStyle,
+                          ))),
+                  rows: dataRows,
+                ),
               ],
             ),
           ),
